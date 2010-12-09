ff0704a86acfc6d8c2c0f86c2bd3a45d9074fd8d
unknown <Daniel@.(none)>
2010-12-09 14:28:05 -0600

    (from Daniel) Took suggestion and got rid of turret cooldown, since it's reduncant now. Also turned destruction back on, since I forgot to do it with my last commit. I think we should return the turret bullets to their original speed, since the firing delay alone seems to take care of the no-time-to-react problem.

--- turret.py
+++ turret.py
@@ -15,14 +15,13 @@ from pygame.mixer import Sound
 TURRET_WIDTH = 24
 TURRET_HEIGHT = 28
 
-GUN_COOLDOWN_TIME = 100
 GUN_CHARGEUP_TIME = 100
 
 class Turret(PhysicalObject):
 	"""This class represents a turret"""
 
 	typ = Type.TURRET
-	timeUntilWeaponCanFireAgain = 0
+	timeLeftToCharge = 0
 	
 
 	def __init__(self, position, level):
@@ -48,7 +47,7 @@ class Turret(PhysicalObject):
 		#print 'turret (x,y) = ', (self.r_x, self.r_y)
 		#print 'turret owner = ', self.controllingPlayer
 
-		self.timeUntilWeaponCanFireAgain = GUN_CHARGEUP_TIME
+		self.timeLeftToCharge = GUN_CHARGEUP_TIME
 
 
 	def step(self, scrollPosition):
@@ -58,11 +57,9 @@ class Turret(PhysicalObject):
 		
 		# update self
 		PhysicalObject.step(self, scrollPosition)
-		if self.timeUntilWeaponCanFireAgain > 0:
-			self.timeUntilWeaponCanFireAgain -= 1
-		if self.timeUntilWeaponCanFireAgain < (1/3.0)*GUN_CHARGEUP_TIME:
+		if self.timeLeftToCharge < (1/5.0)*GUN_CHARGEUP_TIME:
 			self.action = "charged 100"
-		elif self.timeUntilWeaponCanFireAgain < GUN_CHARGEUP_TIME:
+		elif self.timeLeftToCharge < (3/5.0)*GUN_CHARGEUP_TIME:
 			self.action = "charged 50"
 		else:
 			self.action = "charged 0"
@@ -77,8 +74,8 @@ class Turret(PhysicalObject):
 					turretSeesShip = True
 					target = o
 			if turretSeesShip:
-				self.timeUntilWeaponCanFireAgain -= 1
-				if self.timeUntilWeaponCanFireAgain <= 0:
+				self.timeLeftToCharge -= 1
+				if self.timeLeftToCharge <= 0:
 					# it's the ship! get it!
 					soundEfx = pygame.mixer.Sound(constants.TURRET_BULLET_SFX)
 					soundEfx.set_volume(0.5)
@@ -86,35 +83,31 @@ class Turret(PhysicalObject):
 					theBullet = bullet.Bullet((self.rect.x + TURRET_WIDTH/2 - bullet.BULLET_WIDTH/2, self.rect.y + (bullet.BULLET_HEIGHT + 6)), "tur")
 					theBullet.controllingPlayer = self.controllingPlayer
 					# old velocity code
-					#deltaX = target.r_x - self.r_x
-					#deltaY = target.r_y - self.r_y
+					#deltaX = o.r_x - self.r_x
+					#deltaY = o.r_y - self.r_y
 					#distance = math.hypot(deltaX, deltaY)
 					#theBullet.v_x = bullet.DEFAULT_SPEED*(deltaX/distance)  # v_x = speed*cos
 					#theBullet.v_y = bullet.DEFAULT_SPEED*(deltaY/distance)  # v_y = speed*sin
 					# new velocity code; apparently tries to divide by zero and take the square root of a negative number
-					#timeToImpact = ((target.r_x*target.v_x + target.r_y*target.v_y + math.sqrt(-pow(target.r_y,2)*(-1 + pow(target.v_x, 2)) + target.r_x*(target.r_x + 2*target.r_y*target.v_x*target.v_y - target.r_x*pow(target.v_y, 2))))/(-1 + pow(target.v_x, 2) + pow(target.v_y, 2)))
-					#theBullet.v_x = (target.r_x + timeToImpact*target.v_x)/timeToImpact
-					#theBullet.v_y = (target.r_y + timeToImpact*target.v_y)/timeToImpact
+					#timeToImpact = ((o.r_x*o.v_x + o.r_y*o.v_y + math.sqrt(-pow(o.r_y,2)*(-1 + pow(o.v_x, 2)) + o.r_x*(o.r_x + 2*o.r_y*o.v_x*o.v_y - o.r_x*pow(o.v_y, 2))))/(-1 + pow(o.v_x, 2) + pow(o.v_y, 2)))
+					#theBullet.v_x = (o.r_x + timeToImpact*o.v_x)/timeToImpact
+					#theBullet.v_y = (o.r_y + timeToImpact*o.v_y)/timeToImpact
 					# new velocity code, mk. II
 					futurepos = (target.r_x, target.r_y)  # Guess that where they'll be in the future is where they are now
 					MY_SPEED = 1.5 + constants.SCROLL_RATE
 					for i in range(0, 4):
 						dist = (futurepos[0] - self.r_x, futurepos[1] - self.r_y)
-						timetotarget = math.hypot(dist[0], dist[1]) / MY_SPEED
+						timetotarget = math.hypot(dist[0], dist[1]) / bullet.DEFAULT_SPEED
 						distcovered = (target.v_x*timetotarget, target.v_y*timetotarget)
 						futurepos = (target.r_x + distcovered[0], target.r_y + distcovered[1])
 					dirNotNormalized = (futurepos[0] - self.r_x, futurepos[1] - self.r_y)
 					dirNormalized = ((dirNotNormalized[0]/math.hypot(dirNotNormalized[0], dirNotNormalized[1]),
 						dirNotNormalized[1]/math.hypot(dirNotNormalized[0], dirNotNormalized[1])))
-					
 					theBullet.v_x = MY_SPEED*dirNormalized[0]
 					theBullet.v_y = MY_SPEED*dirNormalized[1]
 					# end of velocity code
+					# end of velocity code
 					self.childObjects.append(theBullet)
-					self.timeUntilWeaponCanFireAgain = GUN_COOLDOWN_TIME + GUN_CHARGEUP_TIME
-			else:
-				if self.timeUntilWeaponCanFireAgain < GUN_CHARGEUP_TIME:
-					self.timeUntilWeaponCanFireAgain = GUN_CHARGEUP_TIME
-	
-	
-
+					self.timeLeftToCharge = GUN_CHARGEUP_TIME
+			else:  # if the turret doesn't see the ship,
+				self.timeLeftToCharge = GUN_CHARGEUP_TIME  # then the turret should power down
