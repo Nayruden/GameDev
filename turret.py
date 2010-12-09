c02052fc4ece1285cc0e2743237022ce78aba701
unknown <Daniel@.(none)>
2010-12-09 00:29:57 -0600

    (from Daniel) Successfully implemented ship-movement prediction for turrets using Brett's iterative algorithm.

--- turret.py
+++ turret.py
@@ -68,15 +68,28 @@ class Turret(PhysicalObject):
 					theBullet = bullet.Bullet((self.rect.x + TURRET_WIDTH/2 - bullet.BULLET_WIDTH/2, self.rect.y + (bullet.BULLET_HEIGHT + 6)), "tur")
 					theBullet.controllingPlayer = self.controllingPlayer
 					# old velocity code
-					deltaX = o.r_x - self.r_x
-					deltaY = o.r_y - self.r_y
-					distance = math.hypot(deltaX, deltaY)
-					theBullet.v_x = bullet.DEFAULT_SPEED*(deltaX/distance)  # v_x = speed*cos
-					theBullet.v_y = bullet.DEFAULT_SPEED*(deltaY/distance)  # v_y = speed*sin
+					#deltaX = o.r_x - self.r_x
+					#deltaY = o.r_y - self.r_y
+					#distance = math.hypot(deltaX, deltaY)
+					#theBullet.v_x = bullet.DEFAULT_SPEED*(deltaX/distance)  # v_x = speed*cos
+					#theBullet.v_y = bullet.DEFAULT_SPEED*(deltaY/distance)  # v_y = speed*sin
 					# new velocity code; apparently tries to divide by zero and take the square root of a negative number
 					#timeToImpact = ((o.r_x*o.v_x + o.r_y*o.v_y + math.sqrt(-pow(o.r_y,2)*(-1 + pow(o.v_x, 2)) + o.r_x*(o.r_x + 2*o.r_y*o.v_x*o.v_y - o.r_x*pow(o.v_y, 2))))/(-1 + pow(o.v_x, 2) + pow(o.v_y, 2)))
 					#theBullet.v_x = (o.r_x + timeToImpact*o.v_x)/timeToImpact
 					#theBullet.v_y = (o.r_y + timeToImpact*o.v_y)/timeToImpact
+					# new velocity code, mk. II
+					futurepos = (o.r_x, o.r_y)  # Guess that where they'll be in the future is where they are now
+					for i in range(0, 4):
+						dist = (futurepos[0] - self.r_x, futurepos[1] - self.r_y)
+						timetotarget = math.hypot(dist[0], dist[1]) / bullet.DEFAULT_SPEED
+						distcovered = (o.v_x*timetotarget, o.v_y*timetotarget)
+						futurepos = (o.r_x + distcovered[0], o.r_y + distcovered[1])
+					dirNotNormalized = (futurepos[0] - self.r_x, futurepos[1] - self.r_y)
+					dirNormalized = ((dirNotNormalized[0]/math.hypot(dirNotNormalized[0], dirNotNormalized[1]),
+						dirNotNormalized[1]/math.hypot(dirNotNormalized[0], dirNotNormalized[1])))
+					theBullet.v_x = bullet.DEFAULT_SPEED*dirNormalized[0]
+					theBullet.v_y = bullet.DEFAULT_SPEED*dirNormalized[1]
+					# end of velocity code
 					self.childObjects.append(theBullet)
 					self.timeUntilWeaponCanFireAgain = GUN_COOLDOWN_TIME
 	
